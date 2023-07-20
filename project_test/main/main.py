e059878f2c5c82cb380ad074ac9442d576d34445
MikeWalder <mike_walder@hotmail.fr>
2023-07-20 21:43:30 +0200

    Add text lines

--- project_test/main/main.py
+++ project_test/main/main.py
@@ -1,3 +1,4 @@
 if __name__ == '__main__':
-    print('Ceci est un premier script Python appelé project_test.')
-    # Nous ajouterons ici de futurs scripts dans les prochaines versions du projet
+    print('Ceci est un tout premier script Python appelé project_test.')
+    # Nous ajouterons ici de futurs scripts dans les prochaines versions du projet à venir
+    print('Ceci est une nouvelle fonctionnalité du projet.')
